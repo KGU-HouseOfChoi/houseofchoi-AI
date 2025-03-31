51edcacd4b399856f23282185a7f29a335466c6e
chyun7114 <chyun7114@naver.com>
2025-03-31 10:59:05 +0900

    feat: chatbot 관련 swagger 추가

--- routes/test_route.py
+++ routes/test_route.py
@@ -9,6 +9,13 @@ db_ns = Namespace('database', description='Database related operations')
 # 데이터베이스 연결 테스트 라우트
 @db_ns.route('/db-test')
 class DBTestResource(Resource):
+    @db_ns.doc(
+        description="DB 연결 test API",
+        responses = {
+            200: "DB 연결 성공",
+            500: "DB 연결 실패"
+        }
+    )
     def get(self):
         try:
             conn = get_capstone_db_connection()
