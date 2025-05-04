2fb3ed8215e5aac8f23c83ad9aeec6efb7df028a
chyun7114 <chyun7114@naver.com>
2025-05-04 15:16:25 +0900

    feat: stt api test 적용

--- utils/stt_utils.py
+++ utils/stt_utils.py
@@ -1,5 +1,7 @@
 import os
+import time
 import requests
+from fastapi import UploadFile
 from dotenv import load_dotenv
 from redis import Redis
 
@@ -7,6 +9,7 @@ load_dotenv()
 
 RETURN_ZERO_CLIENT = os.getenv('RETURN_ZERO_CLIENT')
 RETURN_ZERO_SECRET = os.getenv('RETURN_ZERO_SECRET')
+RETURN_ZERO_URL = os.getenv('RETURN_ZERO_URL')
 RETURN_ZERO_JWT_URL = os.getenv('RETURN_ZERO_JWT_URL')
 RETURN_ZERO_TOKEN_KEY = os.getenv('RETURN_ZERO_TOKEN_KEY')
 
@@ -34,3 +37,53 @@ def fetch_token_from_return_zero(redis: Redis) -> str:
     redis.setex(RETURN_ZERO_TOKEN_KEY, 60*60*6, token)
 
     return token
+
+async def try_stt(audio_file: UploadFile, redis: Redis) -> dict:
+    # 토큰 가져오기 (레디스 캐시 활용)
+    token = fetch_token_from_return_zero(redis)
+    if isinstance(token, bytes):
+        token = token.decode()
+
+    # 파일 바이트 읽기
+    audio_bytes = await audio_file.read()
+
+    # multipart/form-data 구성
+    files = {
+        "file": (audio_file.filename, audio_bytes, audio_file.content_type),
+        "config": (None, '{"model_name": "whisper", "language": "ko"}')
+    }
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+
+    # STT API 요청
+    response = requests.post(RETURN_ZERO_URL, headers=headers, files=files)
+
+    # 응답 결과 반환
+    if response.status_code != 200:
+        raise Exception(f"STT request failed: {response.status_code}, {response.text}")
+
+    result = response.json()
+
+    max_attempts = 30
+    interval = 2
+
+    for _ in range(max_attempts):
+        stt_result_reponse = requests.get(
+            'https://openapi.vito.ai/v1/transcribe/' + result["id"],
+            headers={"Authorization": f"Bearer {token}"},
+        )
+
+        if stt_result_reponse.status_code != 200:
+            raise Exception(f"STT request failed: {response.status_code}, {response.text}")
+
+        stt_result = stt_result_reponse.json()
+
+        if stt_result["status"] == "completed":
+            return stt_result
+        elif stt_result["status"] == "failed":
+            raise Exception(f"STT request failed: {response.status_code}, {response.text}")
+        else:
+            time.sleep(interval)
