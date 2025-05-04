7385e19e9ad7532a723855fbad9ba5231398fc0c
chyun7114 <chyun7114@naver.com>
2025-05-04 14:08:22 +0900

    feat: redis DI 추가 및 test controller작성

---
+++ utils/redis_utils.py
@@ -0,0 +1,13 @@
+import os
+
+import redis
+from dotenv import load_dotenv
+
+load_dotenv()
+
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = int(os.getenv("REDIS_PORT"))
+REDIS_DB = int(os.getenv("REDIS_DB"))
+
+def get_redis_client() -> redis.Redis:
+    return redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
